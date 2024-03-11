e22ecae5b86f0dcdf1d4ee87c212181b590a03cf
pooas <cyroustrader@gmail.com>
2024-03-11 13:09:38 +0330

    pointer initiziling

--- Struct.cpp
+++ Struct.cpp
@@ -8,7 +8,14 @@ struct rec{
 
 int main(){
 
-    rec r1{};
+    rec r1{1,2,'c'};
+
+    std::cout << r1.len << '\n';
+
+    rec* ptr = &r1;
+
+    std::cout << r1.len << '\n';
+
     std::cout << sizeof(r1) << "\n";
     return 0;
 }

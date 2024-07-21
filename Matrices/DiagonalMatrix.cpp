680d9d1cf5bb231deb0019962d475e20cbefc059
pooas <cyroustrader@gmail.com>
2024-07-21 16:58:31 +0330

    Implementing DiagonalMatrix in CPP

--- Matrices/DiagonalMatrix.cpp
+++ Matrices/DiagonalMatrix.cpp
@@ -4,6 +4,41 @@
 
 
 #include <stdio.h>
+#include <iostream>
+
+//struct Matrix
+//{
+//    int A[10];
+//    int n;
+//};
+//
+//void Set(struct Matrix *m, int i, int j, int x)
+//{
+//    if(i == j)
+//        m->A[i-1] = x;
+//}
+//
+//int Get(struct Matrix *m, int i, int j)
+//{
+//    if(i == j)
+//        return m->A[i-1];
+//    else
+//        return 0;
+//}
+//
+//void Display(struct Matrix m)
+//{
+//    int i,j;
+//
+//    for (int i = 0; i < m.n; ++i)
+//        for (int j = 0; j < m.n; ++j) {
+//            if(i == j)
+//                printf("%i", m.A[i-1]);
+//            else
+//                printf(" 0 ");
+//        }
+//    printf("\n");
+//}
 
 struct Matrix
 {
@@ -17,26 +52,28 @@ void Set(struct Matrix *m, int i, int j, int x)
         m->A[i-1] = x;
 }
 
-int Get(struct Matrix *m, int i, int j)
-{
-    if(i == j)
+int Get(struct Matrix *m, int i, int j) {
+    if (i == j)
         return m->A[i-1];
     else
         return 0;
+        std::cout << "the number of i and j are not same, Please pass same i and j ...!" << std::endl;
 }
 
-void Display(struct Matrix m)
+int Display(struct Matrix m)
 {
-    int i,j;
+    for (int i = 0; i < m.n; ++i) {
 
-    for (int i = 0; i < m.n; ++i)
-        for (int j = 0; j < m.n; ++j) {
-            if(i == j)
-                printf("%i", m.A[i-1]);
+
+        for (int k = 0; k < m.n; ++k) {
+            if (i == k)
+                printf("%d", m.A[i]);
             else
-                printf(" 0 ");
+                printf("0 ");
+
         }
-    printf("\n");
+        printf("\n");
+    }
 }
 
 int main()
@@ -49,9 +86,9 @@ int main()
     Set(&m, 3,3,6);
     Set(&m, 4,4,12);
 
+
+//    printf("%d \n", Get(&m,2,2));
+
     Display(m);
-
-    printf("%d \n", Get(&m,2,2));
-
     return 0;
 }

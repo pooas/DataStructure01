5301a90b7bcad34254471a4b03636fef29ed7bf5
pooas <cyroustrader@gmail.com>
2024-07-29 13:11:52 +0330

    Implementing Node with using Recursion concept in implementing

--- Linked_list/Node.cpp
+++ Linked_list/Node.cpp
@@ -44,13 +44,58 @@ void display(struct Node *node)
     }
 }
 
+void RDiplay(struct Node *p)
+{
+    while(p->next!=NULL)
+    {
+        printf("%d", p->data);
+        RDiplay(p->next);
+    }
+
+
+}
+
+int count(struct Node* p)
+{
+
+    int l = 0;
+    while(p){
+        l++;
+        p->next;
+    }
+    return l;
+}
+
+int sum(struct Node *p)
+{
+    int s =0;
+    while(p!=NULL)
+    {
+        s +=p->data;
+        p = p->next;
+    }
+    return s;
+}
+
+int RSum(struct Node *p)
+{
+    if(p==NULL)
+        return 0;
+    else
+        return RSum(p->next) + p->data;
+}
 
 int main()
 {
-    struct Node* p;
-    int A[] = {12, 16, 27, 6, 9, 24, 33, 66};
-    creat(A, 8);
-    display(first);
+
+    int A[] = {12, 15, 18, 21, 24,27};
+    creat(A,6);
+    printf("Count %d\n", count(first));
+    printf("Count %d", RSum(first));
+//    int A[] = {12, 16, 27, 6, 9, 24, 33, 66};
+//    creat(A, 8);
+//    display(first);
+//    RDiplay(first);
 
     return 0;
 }

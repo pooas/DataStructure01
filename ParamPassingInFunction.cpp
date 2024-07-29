fb19b8e41e53e34afd17f9264be652b61e4e2570
pooas <cyroustrader@gmail.com>
2024-07-29 13:10:14 +0330

    Implementing ParamPassingInFunction.cpp in cpp

--- ParamPassingInFunction.cpp
+++ ParamPassingInFunction.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
+//
+//void swap(int *x, int *y)
+//{
+//    int temp;
+//    temp = *x;
+//    *x = *y;
+//    *y = temp;
+//}
+//
+//int main(){
+//
+//    int a{10};
+//    int b{20};
+//
+//    swap(&a,&b);
+//
+//    // If we pass by Value, The result is a = 10 , b = 20 swap function seems not working well.
+//    // But, if we pass by reference function working well.
+//    std::cout << a << "   " << b << '\n';
+//
+//
+//
+//
+//
+//    return 0;
+//}
 
-void swap(int *x, int *y)
+void swap(int &a, int &b)
 {
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
+
+    int c;
+    c = a;
+    a = b;
+    b = c;
 }
 
-int main(){
+int main()
+{
+    int a = 12;
+    int b =24;
 
-    int a{10};
-    int b{20};
+    swap(a,b);
 
-    swap(&a,&b);
-
-    // If we pass by Value, The result is a = 10 , b = 20 swap function seems not working well.
-    // But, if we pass by reference function working well.
-    std::cout << a << "   " << b << '\n';
-
-
-
-
-
-    return 0;
+    std::cout << a << " " << b << std::endl;
 }
